675fc7b08c081126e2734ee6220369ea96b067ec
Jovan Zoric <jzjovanzoric@gmail.com>
2021-02-02 20:06:27 +0100

    Objective 1 complete

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -2,13 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Movie;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Movie::class, function (Faker $faker) {
     return [
         'title' => $faker->title,
         'director' => $faker->name,
+        'imageUrl' => $faker->imageUrl,
+        'duration' => rand(10,300),
+        'release_date' => $faker->date,
+        'genre' => $faker->word
     ];
 });
 
--- database/seeds/MoviesTableSeeder.php
+++ database/seeds/MoviesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \Database\MovieFactory;
 
 class MoviesTableSeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class MoviesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // $this->call(MoviesTableSeeder::class, 10);
+        factory(App\Movie::class, 10)->create();
     }
 }
+
+// ->each(function ($movie) {
+//     $movie->posts()->save(factory(App\Post::class)->make());
+// })
